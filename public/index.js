28e4f4ca7f1e4ba370803b8e20c22f016b74b013
Michael <mdurfey@me.com>
2017-05-05 22:09:57 -0700

    added accordian materialize feature

--- public/index.js
+++ public/index.js
@@ -1,43 +1,208 @@
 $('document').ready( () => { 
 $(".dropdown-button").dropdown();
+$('.collapsible').collapsible();
 
-$.get("/fccChannel", function(d){ 
-  // d = JSON.parse(d);
-  console.log(d[0]);
-  var image = d[0][0].logo;
-  var name = d[0][0].display_name;
-  var url = d[0][0].url;
-  $(".fcc").html(`<img class='logoImage' src='${image}'><img><div class="name text-center">${name}</div>`)
-})
+$(".channel").css("cursor", "pointer");
 
-$.get("/music4Studying", function(d){ 
-  console.log(d[0]);
-  var image = d[0][0].logo;
-  var name = d[0][0].display_name;
-  var url = d[0][0].url;
-  var stream = d[0][1].stream;
-  $(".music4Studying").html(`<img class='logoImage' src='${image}'><img><div class="name text-center">${name}</div>`)
-})
 
-$.get("/twitch", function(d){ 
-  console.log(d[0]);
-  var image = d[0][0].logo;
-  var name = d[0][0].display_name;
-  var url = d[0][0].url;
-  var stream = d[0][1].stream;
-  $(".twitch").html(`<img class='logoImage' src='${image}'><img><div class="name text-center">${name}</div>`)
-  // $(".twitch").text(d.display_name);
-})
+var data = []
+$.when(
+  $.get("/fccChannel", function(d) { 
+    console.log(d)
+    data.push(d)
+  }),
+  $.get("/music4Studying", function(d) { 
+    console.log(d)
+    data.push(d)
+  }),
+  $.get("/twitch", function(d) { 
+    console.log(d)
+    data.push(d)
+  }),
+  $.get("/food", function(d) { 
+    console.log(d)
+    data.push(d)
+  }),
+  ).done(function(d) {
+    console.log(d)
+    console.log(data)
+  var fcc = data[0][0];
+  var music = data[1][0];
+  var twitch = data[2][0];
+  var food = data[3][0];
 
-$.get("/food", function(d){ 
-  console.log(d[0]);
-  var image = d[0][0].logo;
-  var name = d[0][0].display_name;
-  var url = d[0][0].url;
-  var stream = d[0][1].stream;
- $(".food").html(`<img class='logoImage' src='${image}'><img><div class="name text-center">${name}</div>`)
-})
+  $(".badge").click( 
+  function() {
+    $(".fccli").child().removeClass("collapsible-header").child().removeClass("collapsible-body");
+  }
+  );a
+  
 
-console.log("we're live")
+  if (fcc[1]["stream"] === null) {
+    console.log("FCC not streaming")
+    var stream = `<div class = "streaming"><span class = "badge right" onclick= "location.href='https://www.twitch.tv/freecodecamp'">Offline</span></div>`;
+    $(".fccli").html(`
+        <div class = "fcc channel collapsible-header" id = "channel" >  
 
+        </div>
+        <div class = "collapsible-body">
+            <span>${fcc[0]["display_name"]} is currently Offline. 
+            <br> Click here to check out ${fcc[0]["display_name"]}'s Twitch Channel
+            </span>
+        </div>
+    `)
+  }
+  else {
+    stream = `
+    <div class = "streaming ">
+      <span class = "new badge" data-badge-caption="">Now Streaming!</span>
+    </div>
+    `
+    $(".fccli").html(`
+      <div class = "fcc channel collapsible-header" id = "channel" >          
+      </div>
+      <div class = "collapsible-body">
+        <span>${fcc[0]["display_name"]} is live! 
+          <br> Click here to check out ${fcc[0]["display_name"]}'s Twitch Channel
+        </span>
+      </div>
+    `)
+  }
+
+  $(".fcc").html(
+    `<img class='logoImage' src='${fcc[0]["logo"]}'><img>
+    <div class="name text-center">${fcc[0]["display_name"]}
+    </div>
+    ${stream}
+    `)
+
+  //FreeCodeCamp//
+if (music[1]["stream"] === null) {
+    console.log("Music not Streaming")
+    var stream = `<div class = "streaming right "><span class = "badge">Offline</span></div>`;
+  $(".musicli").html(`
+    <div class = "music channel collapsible-header" id = "channel" >          
+          </div>
+          <div class = "collapsible-body">
+            <span>${music[0]["display_name"]} is currently Offline. 
+            <br> Click here to check out ${music[0]["display_name"]}'s Twitch Channel
+            </span>
+          </div>
+    `)
+  }
+  else {
+    stream = `
+    <div class = "streaming ">
+      <span class = "new badge" data-badge-caption="">Now Streaming!
+      </span>
+    </div>
+    `
+    $(".musicli").html(`
+      <div class = "music4Studying channel collapsible-header" id = "channel" >          
+      </div>
+      <div class = "collapsible-body">
+        <span>${music[0]["display_name"]} is live! 
+        <br> Click here to check out ${music[0]["display_name"]}'s Twitch Channel
+        </span>
+      </div>
+      `)
+  }
+
+  $(".music4Studying").html(`
+    <img class='logoImage' src='${music[0]["logo"]}'><img>
+      <div class="name text-center">
+        ${music[0]["display_name"]}
+      </div>
+    ${stream}
+    `)
+  //Music4Studying//
+
+  if (twitch[1]["stream"] === null) {
+    console.log("twitch not streaming")
+    var stream = `
+    <div class = "streaming right ">
+      <span class = "badge">
+        Offline
+      </span>
+    </div>`;
+    $(".twitchli").html(`
+      <div class = "twitch channel collapsible-header" id = "channel" >          
+      </div>
+      <div class = "collapsible-body">
+        <span>${twitch[0]["display_name"]} is currently Offline. 
+          <br> Click here to check out ${twitch[0]["display_name"]}'s Twitch Channel
+        </span>
+      </div>
+      `)
+  }
+  else {
+    stream = `
+    <div class = "streaming ">
+      <span class = "new badge" data-badge-caption="">
+        Now Streaming!
+      </span>
+    </div>
+    `
+    $(".twitchli").html(`
+      <div class = "twitch channel collapsible-header" id = "channel" >          
+      </div>
+      <div class = "collapsible-body">
+        <span>${twitch[0]["display_name"]} is live! 
+        <br> Click here to check out ${twitch[0]["display_name"]}'s Twitch Channel
+        </span>
+      </div>
+    `)
+  }
+  $(".twitch").html(`
+    <img class='logoImage' src='${twitch[0]["logo"]}'></img>
+    <div class="name text-center">
+      ${twitch[0]["display_name"]}
+    </div>
+      ${stream}
+  `)
+  //twitch//
+  if (food[1]["stream"] === null) {
+    console.log("food not streaming")
+    var stream = `
+      <div class = "streaming right ">
+        <span class = "badge">
+          Offline
+        </span>
+      </div>`;
+    $(".foodli").html(`
+      <div class = "food channel collapsible-header" id = "channel" >          
+      </div>
+      <div class = "collapsible-body">
+        <span>${food[0]["display_name"]} is currently Offline. 
+        <br> Click here to check out ${food[0]["display_name"]}'s Twitch Channel
+        </span>
+      </div>
+    `)
+  }
+  else {
+    stream = `
+      <div class = "streaming ">
+        <span class = "new badge" data-badge-caption="">
+          Now Streaming!
+        </span>
+      </div>
+    `
+    $(".foodli").html(`
+      <div class = "food channel collapsible-header" id = "channel" >          
+      </div>
+      <div class = "collapsible-body">
+        <span>${food[0]["display_name"]} is live! <br> 
+        Click here to check out ${food[0]["display_name"]}'s Twitch Channel
+        </span>
+      </div>
+      `)
+  }
+  $(".food").html(`<img class='logoImage' src='${food[0]["logo"]}'></img>
+    <div class="name text-center">
+      ${food[0]["display_name"]}
+    </div>
+      ${stream}
+    `)
+  //food//
+  });
 });
